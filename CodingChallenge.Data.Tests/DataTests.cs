d95ffdd2516016bb3b8cd0d9813fad8e2b9b8700
Duilio Mamani <duilio.mamani@incluit.com>
2022-10-13 06:13:42 -0300

    Remove Unnecesary Package

--- CodingChallenge.Data.Tests/DataTests.cs
+++ CodingChallenge.Data.Tests/DataTests.cs
@@ -14,9 +14,9 @@ namespace CodingChallenge.Data.Tests
         {
             //Arrange
             IList<IGeometricForm> geometricForms = new List<IGeometricForm>();
-            IReportingService printService = new ReportingService(new PrintSpanish());
+            IReportingService reportingSvc = new ReportingService(new PrintSpanish());
             //Act
-            string result = printService.Print(geometricForms);
+            string result = reportingSvc.Print(geometricForms);
             //Assert
             Assert.AreEqual("<h1>Lista vacía de formas!</h1>", result);
         }
@@ -26,9 +26,9 @@ namespace CodingChallenge.Data.Tests
         {
             //Arrange
             IList<IGeometricForm> geometricForms = new List<IGeometricForm>();
-            IReportingService printService = new ReportingService(new PrintEnglish());
+            IReportingService reportingSvc = new ReportingService(new PrintEnglish());
             //Act
-            string result = printService.Print(geometricForms);
+            string result = reportingSvc.Print(geometricForms);
             //Assert
             Assert.AreEqual("<h1>Empty list of shapes!</h1>", result);
         }
@@ -38,9 +38,9 @@ namespace CodingChallenge.Data.Tests
         {
             //Arrange
             IList<IGeometricForm> squares = new List<IGeometricForm> { new Square(5) };
-            IReportingService printService = new ReportingService(new PrintSpanish());
+            IReportingService reportingSvc = new ReportingService(new PrintSpanish());
             //Act
-            string result = printService.Print(squares);
+            string result = reportingSvc.Print(squares);
             //Assert
             Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 Formas Perimetro 20 Area 25", result);
         }
@@ -55,9 +55,9 @@ namespace CodingChallenge.Data.Tests
                 new Square(1),
                 new Square(3)
             };
-            IReportingService printService = new ReportingService(new PrintEnglish());
+            IReportingService reportingSvc = new ReportingService(new PrintEnglish());
             //Act
-            string result = printService.Print(squares);
+            string result = reportingSvc.Print(squares);
             //Assert
             Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 Shapes Perimeter 36 Area 35", result);
         }
@@ -76,9 +76,9 @@ namespace CodingChallenge.Data.Tests
                 new Circle(2.75m),
                 new EquilateralTriangle(4.2m)
             };
-            IReportingService printService = new ReportingService(new PrintEnglish());
+            IReportingService reportingSvc = new ReportingService(new PrintEnglish());
             //Act
-            string result = printService.Print(geometricForms);
+            string result = reportingSvc.Print(geometricForms);
             //Assert
             Assert.AreEqual(
                 "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>TOTAL:<br/>7 Shapes Perimeter 97,66 Area 91,65",
@@ -99,9 +99,9 @@ namespace CodingChallenge.Data.Tests
                 new Circle(2.75m),
                 new EquilateralTriangle(4.2m)
             };
-            IReportingService printService = new ReportingService(new PrintSpanish());
+            IReportingService reportingSvc = new ReportingService(new PrintSpanish());
             //Act
-            string result = printService.Print(geometricForms);
+            string result = reportingSvc.Print(geometricForms);
             //Assert
             Assert.AreEqual(
                 "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 Formas Perimetro 97,66 Area 91,65",
@@ -124,9 +124,9 @@ namespace CodingChallenge.Data.Tests
                 new EquilateralTriangle(4.2m),
                 new Rectangle(4,8)
             };
-            IReportingService printService = new ReportingService(new PrintSpanish());
+            IReportingService reportingSvc = new ReportingService(new PrintSpanish());
             //Act
-            string result = printService.Print(geometricForms);
+            string result = reportingSvc.Print(geometricForms);
             //Assert
             Assert.AreEqual(
                 "<h1>Reporte de Formas</h1>" +
